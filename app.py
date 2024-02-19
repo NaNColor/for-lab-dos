be0b395c6cab7dc56258a1919aa73d56a17e7076
Rosamon <55803598+Rosamon@users.noreply.github.com>
2024-02-19 16:15:11 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+load_dotenv()
+
+PORT = int(os.getenv("PORT", 8080))
+HOST = os.getenv("HOST", '0.0.0.0')
+SECRET_KEY = os.getenv("SECRET_KEY")
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
+
+@app.route('/data')
+def get_data():
+    with open('data.json', 'r') as file:
+        data = file.read()
+    return jsonify(data)
+
+@app.route('/secrets')
+def get_secrets():
+    return jsonify({
+        "secretKey": SECRET_KEY,
+        "user": USER,
+        "password": PASSWORD
+    })
+
+if __name__ == '__main__':
+    app.run(host=HOST, port=PORT)
